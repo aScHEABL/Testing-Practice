1a75b16a73e1d5e6a1443863557989dd0824a2bb
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-12-17 22:10:23 +0800

    update: complete calculator

---
+++ src/scripts/calculator.test.js
@@ -0,0 +1,16 @@
+const calculator = require("./calculator");
+
+describe("A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.", () => {
+  test("Works with addition", () => {
+    expect(calculator.add(2, 8)).toBe(10);
+  })
+  test("Works with subtraction", () => {
+    expect(calculator.subtract(6, 3)).toBe(3);
+  })
+  test("Works with division", () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+  })
+  test("Works with multiplication", () => {
+    expect(calculator.multiply(100, 100)).toBe(10000);
+  })
+})
