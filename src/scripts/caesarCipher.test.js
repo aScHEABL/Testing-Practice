2a617a80331388a091c05a40bc733bd06107a3a5
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-12-19 08:04:22 +0800

    update: caesarCipher.js WIP #0

---
+++ src/scripts/caesarCipher.test.js
@@ -0,0 +1,7 @@
+const caesarCipher = require("./caesarCipher");
+
+describe("A function that takes a string and returns it with each character “shifted”.", () => {
+    test("Character wrapping from z to a", () => {
+        expect(caesarCipher("by so changing the order", 7)).toBe("if johunpun aol vykly");
+    })
+})
