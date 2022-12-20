a3060f444a6e984d8bf2141bced5e0c1192a7d72
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-12-20 09:57:18 +0800

    update: caesarCipher WIP #2

--- src/scripts/caesarCipher.js
+++ src/scripts/caesarCipher.js
@@ -3,26 +3,19 @@ function caesarCipher(string, shift) {
     let encriptedString = "";
     const stringToArray = string.split("");
     stringToArray.forEach((character) => {
-        // Get the ASCII code of every character of the array.
-        let charCode = character.charCodeAt(0);
-        // If the character is not a-z || A-Z then just concat into the string.
-        if (!character.match(/[a-zA-Z]/)) encriptedString =+ character;
+        let charCode = character.charCodeAt(0)
+        // console.log(character)
+        // console.log(charCode);
+        if (shift > 25) shift %= 26;
+        // If the character is not from a-z, A-Z then concat it directly.
+        if (!character.match(/[a-zA-Z]/)) encriptedString += character;
         else {
-            let upperOrLower = (charCode <= 90) ? "upperCase" : "lowerCase";
-            console.log(`Character: ${character}`);
-            console.log(`Shift: ${charCode} + ${shift}`);
-            if (upperOrLower === "upperCase") {
-                if (charCode + shift > 90 || charCode + shift < 65) {
-                    encryptedString += String.fromCharCode((charCode + shift + 65) % 26 + 65);
-                }
-            } else if (upperOrLower === "lowerCase") {
-                if (charCode + shift > 122 || charCode + shift < 97) {
-                    encriptedString += String.fromCharCode((charCode + shift + 97) % 26 + 97);
-                }
-            }
+            
         }
     })
     return encriptedString;
 }
+
+console.log(caesarCipher("changing", 7));
 
 module.exports = caesarCipher;
--- src/scripts/caesarCipher.test.js
+++ src/scripts/caesarCipher.test.js
@@ -2,6 +2,6 @@ const caesarCipher = require("./caesarCipher");
 
 describe("A function that takes a string and returns it with each character “shifted”.", () => {
     test("Character wrapping from z to a", () => {
-        expect(caesarCipher("by so changing the order", 7)).toBe("if johunpun aol vykly");
+        expect(caesarCipher("by changing the order", 7)).toBe("if johunpun aol vykly");
     })
 })
